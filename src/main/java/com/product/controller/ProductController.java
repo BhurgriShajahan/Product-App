264319c21ecb63fc73232ee22545cb0dceecf127
Shahjahan <146447169+BhurgriShajahan@users.noreply.github.com>
2024-11-27 10:13:11 -0700

    Work in Product Controller.

--- src/main/java/com/product/controller/ProductController.java
+++ src/main/java/com/product/controller/ProductController.java
@@ -5,6 +5,8 @@ import com.product.model.custom.CustomResponseEntity;
 import com.product.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/v1/product")
@@ -13,19 +15,22 @@ public class ProductController {
     @Autowired
     ProductService productService;
 
-    //Create Product
+    // Create Product
     @PostMapping("/create")
-    public CustomResponseEntity<?> createNewProduct(@RequestBody ProductDto productDto)
-    {
-         return productService.createNewProduct(productDto);
+    public CustomResponseEntity<?> createNewProduct(@RequestBody ProductDto productDto) {
+        return productService.createNewProduct(productDto);
     }
 
-    //Fetch All Products
+    // Fetch All Products
     @GetMapping("/fetchAll")
-    public CustomResponseEntity fetchAllProducts()
-    {
+    public CustomResponseEntity fetchAllProducts() {
         return productService.fetchAllProducts();
     }
 
+    @ResponseBody
+    @GetMapping("/msg")
+    String message() {
+        return "Product Controller started..";
+    }
 
 }
